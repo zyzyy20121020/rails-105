266845f949c882b5c3200e8864a111eb72977322
ReadingPig <zyzpop830203@163.com>
2017-06-02 19:57:09 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,9 +9,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
-
-    redirect_to groups_path
+    
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def show
